6c09a688e78744149804f23e3b48d75e56d2fa50
Priyesh Gohil <priyeshgohil21@gmail.com>
2025-12-14 14:20:17 +0530

    initial commit

---
+++ server/src/controllers/productController.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+
+const prisma = new PrismaClient();
+
+export const getProducts = async (
+    req: Request,
+    res: Response
+    ) : Promise<void> => {
+    try {
+      const search = req.query.search?.toString();
+      const products = await prisma.products.findMany({
+        where: {
+          name: {
+            contains: search,
+            mode: "insensitive"
+          },
+        },
+      });
+      res.json(products);
+    } catch (error) {
+      res.status(500).json({ message: "Error retreiving products" });
+    }
+}
+
+
+export const createProduct = async (
+    req: Request,
+    res: Response
+    ) : Promise<void> => {
+    try {
+      const { productId, name, price, rating, stockQuantity } = req.body;
+      const product = await prisma.products.create({
+        data: {
+          productId,
+          name,
+          price,
+          rating,
+          stockQuantity,
+        },
+      });
+      res.status(201).json(product)
+    } catch (error) {
+      res.status(500).json({ message: "Error creating product" });
+    }
+}
+
+
